6dfac4df426fbddbf47c45479101958fc2395d4a
Jonathan Card <joncard93@hotmail.com>
2015-06-13 21:17:27 -0400

    Changes to Breeder. Don't remember what they are.

--- Breeder.py
+++ Breeder.py
@@ -1,23 +1,19 @@
-# 2015.06.08 21:32:53 EDT
-# Embedded file name: /cygdrive/c/Users/Jonathan/git/2048/Breeder.py
 import random
 from strategies import GeneticStrategy
 from Player import Player
+
+MAX_WEIGHT = (1 << 12) - 1
+NUM_BREAKS = 2
 
 class Breeder(object):
 
     def start(self):
         strategies = []
-        for j in range(0, 100):
-            for i in range(0, 10):
-                gene_code = ''
-                for j in range(0, 16):
-                    weight = random.randint(0, 65535)
-                    gene_code += '{0:0>16b}'.format(weight)
+        strategies.extend(self.generate_random(10))
 
-                strategies.append(GeneticStrategy(gene_code))
-
+	for j in range(0, 100):
             for strategy in strategies:
+		strategy.score = 0
                 for i in range(0, 10):
                     player = Player()
                     player.strategy = strategy
@@ -38,33 +34,61 @@ class Breeder(object):
             print 'Best score: %d' % sorted_strategies[0].score
             print 'Second score: %d' % sorted_strategies[1].score
             print '\n'
-            strategies = self.breed(sorted_strategies[0], sorted_strategies[1])
+            strategies = [sorted_strategies[0], sorted_strategies[1]]
+            strategies.extend(self.breed(sorted_strategies[0], sorted_strategies[1], 2))
+            strategies.extend(self.generate_random(2))
+            strategies.append(self.mutate(strategies[4]))
+            strategies.append(self.mutate(strategies[5]))
             print 'New Strategies...'
 
-    def breed(self, candidate1, candidate2):
-        strategies = [candidate1, candidate2]
-        for i in range(0, 8):
+    def breed(self, candidate1, candidate2, count):
+        strategies = []
+        for i in range(0, count):
             breaks = []
-            for i in range(0, 4):
+            for i in range(0, NUM_BREAKS):
                 breaks.append(random.randint(0, 255))
 
             sorted_breaks = sorted(breaks)
             print sorted_breaks
-            new_gene_code = ''
-            parent = candidate1
+            new_gene_code1 = ''
+            new_gene_code2 = ''
+            parents = [candidate1, candidate2]
             break_index = 0
-            for i in range(0, 256):
+            for i in range(0, len(candidate1.gene_code)):
                 if i == sorted_breaks[break_index]:
-                    if parent == candidate1:
-                        parent = candidate2
+                    if parents[0] == candidate1:
+                        parents[0] = candidate2
+                        parents[1] = candidate1
                     else:
-                        parent = candidate1
-                new_gene_code += parent.gene_code[i]
+                        parents[0] = candidate1
+                        parents[1] = candidate2
+                new_gene_code1 += parents[0].gene_code[i]
+                new_gene_code2 += parents[1].gene_code[i]
 
-            print new_gene_code
-            strategies.append(GeneticStrategy(new_gene_code))
+            print "Child 1: %s" % new_gene_code1
+            print "Child 2: %s" % new_gene_code2
+            strategies.append(GeneticStrategy(new_gene_code1))
+            strategies.append(GeneticStrategy(new_gene_code2))
 
         return strategies
-# okay decompyling Breeder.pyc 
-# decompiled 1 files: 1 okay, 0 failed, 0 verify failed
-# 2015.06.08 21:32:53 EDT
+
+    def generate_random(self, count):
+        strategies = []
+        for i in range(0, count):
+            gene_code = ''
+            for j in range(0, 16):
+                weight = random.randint(0, MAX_WEIGHT)
+                gene_code += '{0:0>16b}'.format(weight)
+
+            strategies.append(GeneticStrategy(gene_code))
+
+        return strategies
+
+    def mutate(self, strategy):
+        mutate_index = random.randint(0, len(strategy.gene_code) - 1)
+        new_gene_code = strategy.gene_code
+        if(new_gene_code[mutate_index] == '1'):
+            new_gene_code = new_gene_code[:mutate_index] + '0' + new_gene_code[mutate_index + 1:]
+        else:
+            new_gene_code = new_gene_code[:mutate_index] + '1' + new_gene_code[mutate_index + 1:]
+        return GeneticStrategy(new_gene_code)
